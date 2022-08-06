5ca892bba2110b7368a2d661577f3cd78a0ccf15
Andrey Dulmaev <dulmaev.andrei@gmail.com>
2022-08-06 17:44:34 +0800

    Add Lesson1

---
+++ Lesson1/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 1: Напишите программу, которая на вход принимает два числа 
+и выдаёт, какое число большее, а какое меньшее. */
+
+Console.WriteLine("Введите первое число:");
+int first = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int second = int.Parse(Console.ReadLine());
+if (first > second)
+{
+    Console.WriteLine($"max = {first}, min = {second}");
+}
+else
+{
+    Console.WriteLine($"max = {second}, min = {first}");
+}
---
+++ Lesson1/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 2: Напишите программу, которая принимает на вход три числа 
+и выдаёт максимальное из этих чисел. */
+
+Console.WriteLine("Введите первое число:");
+int first = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int second = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число:");
+int third = int.Parse(Console.ReadLine());
+if (first >= second && first >= third)
+{
+    Console.WriteLine($"max = {first}");
+}
+else if (second >= first && second >= third)
+{
+        Console.WriteLine($"max = {second}");
+}
+else 
+{
+    Console.WriteLine($"max = {third}");
+}
